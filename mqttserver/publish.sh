3a87eb521e115ea849248d0260ffeb0701de6470
ericmschow <eric.m.schow@gmail.com>
2019-12-31 17:49:29 -0600

    obviously the publish script should unpack files, what was I thinking

--- mqttserver/publish.sh
+++ mqttserver/publish.sh
@@ -10,4 +10,5 @@ echo 'Transferring...'
 scp ./build.tar pi@$MQTT_PI_IP:/home/pi/mqttserver
 echo 'Cleaning up...'
 rm ./build.tar
-echo 'Extract at target location with tar -xf in case you forgot the basic command'
+echo 'Extracting at destination...'
+ssh pi@$MQTT_PI_IP "cd /home/pi/mqttserver && tar -xf ./build.tar"
