13f43300edb796e49ca891b18246f147d3ee7fb8
ericmschow <eric.m.schow@gmail.com>
2019-12-31 19:19:33 -0600

    general bugfixes discovered during first real testing run

--- mqttserver/index.js
+++ mqttserver/index.js
@@ -1,34 +1,65 @@
-const MqttHandler = require('./MqttHandler');
-const Promise = require('bluebird');
 
+const Promise = require('bluebird');
 const express = require('express')
+
+const MqttHandler = require('./MqttHandler');
+const CoffeeController = require('./controllers/CoffeeController');
+const LampController = require('./controllers/LampController');
+
 const app = express()
 const port = process.env.MQTTSERVER_LISTENING_PORT;
 
-const mqttHandler = new MqttHandler();
-app.mqttHandler = await mqttHandler.initialize();
+app.log = function() {
+  // in case I ever need this thing logging somewhere
+  let msg = [...arguments].join(' ');
+  console.log(msg);
+};
 
-app.get('/', (req, res) => res.send('Hello World!'))
+(() => {
+  async function start() {
+    try {
+      app.mqttHandler = new MqttHandler();
+      await app.mqttHandler.initialize();
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+      app.controllers = {
+        coffee: new CoffeeController(app),
+        lamp: new LampController(app)
+      };
 
-// let mqttHandler;
-// (() => {
-//   async function start() {
-//     try {
-//       await mqttHandler.publishMessage('lamp', 'Power', 'TOGGLE');
-//       await Promise.delay(5000)
-//       await mqttHandler.publishMessage('lamp', 'Power', 'TOGGLE');
-//       console.log('tearing down');
-//       await mqttHandler.teardown();
-//     }
-//     catch (e) {
-//       console.error(e);
-//       if (mqttHandler) {
-//         await mqttHandler.teardown();
-//       }
-//     }
-//   };
-//   console.log('starting');
-//   start();
-// })();
+      app.get('/coffee', async (req, res) => {
+        try {
+          res.send('Agitation request received');
+          await (app.controllers.coffee.agitateWater());
+          res.send('Agitation complete if you\'re still listening.');
+        }
+        catch (err) {
+          app.log('coffee error', err);
+          res.status(500);
+        }
+      })
+
+      app.get('/lamp', async (req, res) => {
+        try {
+          await (app.controllers.lamp.toggleOnce());
+          res.send('Lamp toggled.');
+        }
+        catch (err) {
+          app.log('lamp error', err);
+          res.status(500);
+        }
+      })
+
+      app.get('/', function (req, res) {
+        res.send('GET request to homepage')
+      })
+
+      app.listen(port, () => console.log(`IOT Server listening on port ${port}!`))
+    }
+    catch (err) {
+      console.error(err);
+      if (app.mqttHandler) await app.mqttHandler.teardown();
+      process.exit(1);
+    }
+  }
+  start();
+})()
