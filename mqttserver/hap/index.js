19a416c01f7fe84bf92472ce6706aeb7c6367158
Erik <you@example.com>
2023-11-29 08:11:51 -0600

    workaround for restricted number of people per home

--- mqttserver/hap/index.js
+++ mqttserver/hap/index.js
@@ -1,7 +1,34 @@
-const coffeeAccessory = require('./coffeeAccessoryBuilder');
-const lampAccessory = require('./lampAccessoryBuilder');
+const hap = require("hap-nodejs");
+const accessoryBuilder = require('./accessoryBuilder');
 
 module.exports = async app => {
-    await coffeeAccessory(app);
-    await lampAccessory(app);
+    const coffee1 = {
+        controller: app.controllers.coffee,
+        name: 'Coffee Pot',
+        port: 47130,
+        category: hap.Categories.OUTLET,
+        enableMethod: app.controllers.coffee.agitateWater.bind(app.controllers.coffee),
+        macAddress: '2A-51-2A-EB-C6-D1'
+    };
+    const coffee2 = Object.assign({}, coffee1, {
+        port: 47132,
+        macAddress: '2A-51-2A-EB-C6-D2'
+    })
+    const lamp1 = {
+        controller: app.controllers.lamp,
+        name: 'Lamp',
+        port: 47129,
+        category: hap.Categories.LIGHTBULB,
+        macAddress: '8C-92-75-40-2D-2E'
+    };
+    const lamp2 = Object.assign({}, lamp1, {
+        port: 47131,
+        macAddress: '8C-92-75-40-2D-2D'
+    })
+    // due to apple gating having multiple people per home behind a device, this is my workaround
+    // double the accessory count, share the controller -> shared state
+    await accessoryBuilder(coffee1);
+    await accessoryBuilder(coffee2);
+    await accessoryBuilder(lamp1);
+    await accessoryBuilder(lamp2);
 }
--- mqttserver/hap/lampAccessoryBuilder.js
+++
@@ -1,9 +0,0 @@
-const hap = require("hap-nodejs");
-const accessoryBuilder = require('./accessoryBuilder');
-
-module.exports = (app) => accessoryBuilder({
-    controller: app.controllers.lamp,
-    name: 'Lamp',
-    category: hap.Categories.LIGHTBULB,
-    macAddress: '8C-92-75-40-2D-2E'
-})
